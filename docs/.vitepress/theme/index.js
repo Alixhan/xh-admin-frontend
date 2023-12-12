60f75d0d1923a893a736383db2998b457d47bbdb
sxh <1609737773@qq.com>
2023-12-12 22:17:41 +0800

    docs(docs): 调整doc编译选项

--- docs/.vitepress/theme/index.js
+++ docs/.vitepress/theme/index.js
@@ -1,26 +1,31 @@
-import { createPinia } from 'pinia'
 import ElementPlus from 'element-plus'
 import * as ElementPlusIconsVue from '@element-plus/icons-vue' // element图标
-import MyComponents from '@/components/index'
-import { AuthDirective } from '@/directive'
 import '@/styles/index.scss' // 样式引入
 import DefaultTheme from 'vitepress/theme'
 import MyLayout from './MyLayout.vue'
-import I18n from '@/i18n/index'
+// import { createPinia } from 'pinia'
+// import MyComponents from '@/components/index'
+// import { AuthDirective } from '@/directive'
+// import I18n from '@/i18n/index'
+
 export default {
   ...DefaultTheme,
   Layout: MyLayout,
   enhanceApp(ctx) {
     const app = ctx.app
-    app.use(I18n)
-    app.use(createPinia())
     app.use(ElementPlus)
     for (const [key, component] of Object.entries(ElementPlusIconsVue)) {
       app.component(key, component)
     }
-    // 全局注册自定义组件
-    app.use(MyComponents)
-    // 全局注册权限自定义指令
-    app.directive('auth', AuthDirective)
+    // app.use(I18n)
+    // app.use(createPinia())
+    // app.use(ElementPlus)
+    // for (const [key, component] of Object.entries(ElementPlusIconsVue)) {
+    //   app.component(key, component)
+    // }
+    // // 全局注册自定义组件
+    // app.use(MyComponents)
+    // // 全局注册权限自定义指令
+    // app.directive('auth', AuthDirective)
   },
 }
