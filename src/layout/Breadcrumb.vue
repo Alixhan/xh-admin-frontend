c34b2cf110938da470eacb18a0c33c4126822a8e
sxh <1609737773@qq.com>
2025-06-05 15:49:23 +0800

    style: 添加面包屑切换动画

--- src/layout/Breadcrumb.vue
+++ src/layout/Breadcrumb.vue
@@ -1,15 +1,18 @@
 <template>
   <el-breadcrumb separator="/">
-    <el-breadcrumb-item v-for="item in systemStore.activeMenuArr" :key="item.fullPath">
-      <div class="tag-content">
-        <m-icon v-show="systemStore.layout.showNavTabIcon && item.icon" class="tab-icon" :value="item.icon" />
-        {{ item.title }}
-      </div>
-    </el-breadcrumb-item>
+    <transition-group name="breadcrumb">
+      <el-breadcrumb-item v-for="item in systemStore.activeMenuArr" :key="item.fullPath">
+        <div class="tag-content">
+          <m-icon v-show="systemStore.layout.showNavTabIcon && item.icon" class="tab-icon" :value="item.icon" />
+          {{ item.title }}
+        </div>
+      </el-breadcrumb-item>
+    </transition-group>
   </el-breadcrumb>
 </template>
 <script setup>
 import { useSystemStore } from '@/stores/system'
+
 defineOptions({
   name: 'DefaultBreadcrumb'
 })
@@ -24,5 +27,16 @@ const systemStore = useSystemStore()
     display: inline-block;
     margin-right: 0.5em;
   }
+}
+
+// breadcrumb animate
+.breadcrumb-enter-active {
+  transition: all 0.5s ease;
+}
+
+.breadcrumb-enter-from,
+.breadcrumb-leave-active {
+  opacity: 0;
+  transform: translateX(10px);
 }
 </style>
