c047103f0714f10b79f8ce28cf3c376ba46441d6
sxh <1609737773@qq.com>
2025-06-16 18:38:31 +0800

    style: 样式微调

--- src/stores/theme.ts
+++ src/stores/theme.ts
@@ -24,8 +24,8 @@ export const useThemeStore = defineStore('theme', () => {
     '--el-color-white': { light: '#ffffff', dark: '#ececec' },
     '--el-color-black': { light: '#02191c', dark: '#1c1c1c' },
 
-    '--el-bg-color-page': { light: '#f5f5f5', dark: '#252525' },
-    '--el-bg-color': { light: '#ffffff', dark: '#1e1e1e' },
+    '--el-bg-color-page': { light: '#f5f5f5', dark: '#2b2b2b' },
+    '--el-bg-color': { light: '#ffffff', dark: '#232323' },
     '--el-bg-color-overlay': { light: '#ffffff', dark: '#1d1e1f' },
 
     '--el-color-primary': { light: '#409eff', dark: '#409eff' },
@@ -269,7 +269,7 @@ export const useThemeStore = defineStore('theme', () => {
         '--el-color-primary-light-3': { light: '#AF90F9', dark: '#8b5df8' },
         '--el-color-primary-light-5': { light: '#C8B2FB', dark: '#9165f5' },
         '--el-color-primary-light-7': { light: '#DCCFFC', dark: '#7f51fa' },
-        '--el-color-primary-light-8': { light: '#EEE7FE', dark: '#543d8d' },
+        '--el-color-primary-light-8': { light: '#EEE7FE', dark: '#402f6a' },
         '--el-color-primary-light-9': { light: '#F8F5FE', dark: '#212028' }
       }
     },
